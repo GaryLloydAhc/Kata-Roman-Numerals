e9effca3a32a03c37ce6da1c49cc2c4c239f0b8e
Gary Lloyd <gary.lloyd@advancedcomputersoftware.com>
2016-11-15 13:33:44 +0000

    #1 very simple code to pass tests. :ant:

--- Kata-Roman-Numerals/RomanNumeralConverter.cs
+++ Kata-Roman-Numerals/RomanNumeralConverter.cs
@@ -1,10 +1,33 @@
-﻿namespace Kata_Roman_Numerals
+﻿using System;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+
+namespace Kata_Roman_Numerals
 {
     public class RomanNumeralConverter
     {
+        public const int INVALID_ROMAN_NUMBER = 0;
+
+        private readonly Dictionary<string, int> _romanValuesLookup = new Dictionary<string, int>
+        {
+            {"I",1 },
+            {"II", 2 },
+            {"III", 3 },
+            {"IV", 4 },
+            {"V", 5 },
+        };
+
         public int ConvertRomanNumeralToDecimal(string romanNumber)
         {
-            return 0;
+            try
+            {
+                return _romanValuesLookup[romanNumber];
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Oops roman number not valid - {romanNumber}");                
+            }
+            return INVALID_ROMAN_NUMBER;
         }
     }
 }
